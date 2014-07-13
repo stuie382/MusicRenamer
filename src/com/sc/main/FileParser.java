e0fdf76453d9949f93fa59df795d923923c2949f
Stuart Clark <stu382@hotmail.co.uk>
2014-07-13 10:11:54 +0100

    got track information

--- src/com/sc/main/FileParser.java
+++ src/com/sc/main/FileParser.java
@@ -1,36 +1,62 @@
 package com.sc.main;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
+import org.apache.tika.metadata.XMPDM;
+import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.mp3.Mp3Parser;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.sc.view.Track;
 
 public class FileParser {
+
     private File inputDirectory;
     private static final Logger log = Logger.getLogger(FileParser.class);
 
     public static void main(String[] args) {
 	log.debug("Input location::" + args[0]);
 	FileParser fileParser = new FileParser(args[0]);
-	fileParser.parse();
+	fileParser.startParse();
     }
 
     public FileParser(final String inputDirectoryLoc) {
 	inputDirectory = new File(inputDirectoryLoc);
-
     }
 
-    public final void parse() {
+    public final void startParse() {
 	File[] listOfFiles = inputDirectory.listFiles();
+	List<Track> inputWithMeta = new ArrayList<>();
+
 	for (File inputFile : listOfFiles) {
 	    try (InputStream input = new FileInputStream(inputFile)) {
 		log.debug("Looking at file::" + inputFile.getAbsolutePath());
+		ContentHandler handler = new DefaultHandler();
+		Metadata metadata = new Metadata();
+		ParseContext parseContext = new ParseContext();
+		Parser parser = new Mp3Parser();
+		parser.parse(input, handler, metadata, parseContext);
 
+		Track newTrack = new Track(
+			metadata.get(TikaCoreProperties.TITLE),
+			metadata.get(XMPDM.TRACK_NUMBER),
+			metadata.get(XMPDM.ALBUM), metadata.get(XMPDM.ARTIST),
+			inputFile);
+		inputWithMeta.add(newTrack);
+		log.debug(newTrack.toString());
 	    } catch (Exception ex) {
 		log.error("Something went wrong::" + ex.getMessage());
 	    }
+
 	}
     }
 }
---
+++ src/com/sc/view/Track.java
@@ -0,0 +1,62 @@
+package com.sc.view;
+
+import java.io.File;
+
+public class Track {
+    private String trackName;
+    private int trackNumber;
+    private String albumName;
+    private String artist;
+    private File content;
+
+    /**
+     * Selection of meta information about the track, and the track content
+     * itself.
+     * 
+     * @param trackName
+     * @param albumName
+     * @param artist
+     * @param content
+     */
+    public Track(final String trackName, final String trackNumber,
+	    final String albumName, final String artist, final File content) {
+	super();
+	this.trackName = trackName;
+	this.trackNumber = Integer.parseInt(trackNumber);
+	this.albumName = albumName;
+	this.artist = artist;
+	this.content = content;
+    }
+
+    public final String getTrackName() {
+	return trackName;
+    }
+    
+    public final int getTrackNumber() {
+	return trackNumber;
+    }
+
+    public final String getAlbumName() {
+	return albumName;
+    }
+
+    public final String getArtist() {
+	return artist;
+    }
+
+    public final File getContent() {
+	return content;
+    }
+
+    @Override
+    public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("Track [trackName=").append(trackName)
+		.append(", trackNumber=").append(trackNumber)
+		.append(", albumName=").append(albumName).append(", artist=")
+		.append(artist).append(", content=").append(content)
+		.append("]");
+	return builder.toString();
+    }
+
+}
